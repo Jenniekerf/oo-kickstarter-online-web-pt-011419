3373cf55e7bb44355570116ac84959564b88d322
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-02-13 18:10:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,6 @@
 class Backer
   
-  attr_reader :name, :backed_projects
+  attr_reader :name, :backed_projects, :backer
   
   def initialize(name)
     @name = name
@@ -9,7 +9,7 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    @backers << project
+  
   end
   
 end
